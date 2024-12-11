547e67b915687635ae4badd80e082ee24d07f9dc
Usama <usamaabakkar@gmail.com>
2024-12-11 21:19:47 +0300

    Fixed and used date picker in Load Factor Report

--- lib/screens/staff/settings/reports/load_factor_report.dart
+++ lib/screens/staff/settings/reports/load_factor_report.dart
@@ -1,10 +1,10 @@
 import "package:provider/provider.dart";
 import "package:flutter/material.dart";
 
+import "package:railway_system/screens/staff/cards/load_factor_card.dart";
 import "package:railway_system/data/train_card_data.dart";
 import "package:railway_system/models/user.dart";
 import "package:railway_system/models/db.dart";
-import "package:railway_system/screens/staff/cards/load_factor_card.dart";
 
 class StaffLoadFactorReport extends StatefulWidget {
   const StaffLoadFactorReport({super.key});
@@ -15,73 +15,16 @@ class StaffLoadFactorReport extends StatefulWidget {
 
 class _StaffLoadFactorReportState extends State<StaffLoadFactorReport> {
   List<TrainCardData> cardsData = [];
+  DateTime? selectedDate;
 
   @override
   void initState() {
-    getDataFromDB();
     super.initState();
-  }
-
-  void getDataFromDB() async {
-    var dbModel = context.read<DBModel>();
-    var now = DateTime.now();
-
-    // Get all train IDs
-
-    var trainsQuery = await dbModel.conn.execute("SELECT ID FROM train");
-    List<TrainCardData> tmpCardsData = [];
-
-    for (var row in trainsQuery.rows) {
-      var trainID = row.colByName("ID")!;
-
-      var searchResult = (await dbModel.conn.execute("""
-    SELECT 
-        pt1.TrainID, pt1.Time AS S_Time, pt2.Time AS F_Time, t.NameEN, t.NameAR, t.BusinessCapacity, t.EconomyCapacity, t.StartsAt_Name, t.EndsAt_Name, t.StartsAt_Time
-    FROM
-        passing_through pt1 JOIN passing_through pt2 ON pt1.TrainID = pt2.TrainID
-            JOIN train t ON pt1.TrainID = t.ID
-    WHERE
-        pt1.TrainID = $trainID AND pt1.StationName = t.StartsAt_Name AND pt2.StationName = t.EndsAt_Name AND pt1.SequenceNo < pt2.SequenceNo 
-    ORDER BY pt1.TrainID, pt1.SequenceNo;
-  """)).rows.toList();
-
-      var bookings = (await dbModel.conn.execute("""
-    SELECT *
-    FROM booking NATURAL JOIN listed_booking
-    WHERE
-    Date = '${now.year}-${now.month}-${now.day}';
-  """)).rows.toList();
-
-      var ptRow = searchResult.first;
-
-      tmpCardsData.add(TrainCardData(
-        trainID: int.parse(ptRow.colByName("TrainID")!),
-        nameEN: ptRow.colByName("NameEN")!,
-        nameAR: ptRow.colByName("NameAR")!,
-        source: ptRow.colByName("StartsAt_Name")!,
-        destination: ptRow.colByName("EndsAt_Name")!,
-        date: "${now.year}-${now.month}-${now.day}",
-        sTime: int.parse(ptRow.colByName("S_Time")!),
-        fTime: int.parse(ptRow.colByName("F_Time")!),
-        businessCapacity: int.parse(ptRow.colByName("BusinessCapacity")!),
-        economyCapacity: int.parse(ptRow.colByName("EconomyCapacity")!),
-        bookedBusiness: bookings.where((booking) => booking.colByName("On_ID") == ptRow.colByName("TrainID") && booking.colByName("Coach") == "Business").length,
-        bookedEconomy: bookings.where((booking) => booking.colByName("On_ID") == ptRow.colByName("TrainID") && booking.colByName("Coach") == "Economy").length,
-      ));
-    }
-
-    //by train ID
-    tmpCardsData.sort((a, b) => a.trainID.compareTo(b.trainID));
-
-    setState(() {
-      cardsData = tmpCardsData;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     var userModel = context.read<UserModel>();
-    var selectedDate;
 
     return Scaffold(
       appBar: PreferredSize(
@@ -158,58 +101,106 @@ class _StaffLoadFactorReportState extends State<StaffLoadFactorReport> {
               ),
             ),
           ),
-          
-            Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 241, 241, 241),
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 5,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: GestureDetector(
-                  onTap: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime.now().add(const Duration(days: 365)),
-                    );
-                    if (pickedDate != null) {
-                      setState(() {
-                        selectedDate = pickedDate;
-                      });
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 241, 241, 241),
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: GestureDetector(
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(const Duration(days: 365)),
+                  );
+                  if (pickedDate != null) {
+                    var dbModel = context.read<DBModel>();
+
+                    // Get all train IDs
+
+                    var trainsQuery = await dbModel.conn.execute("SELECT ID FROM train");
+                    List<TrainCardData> tmpCardsData = [];
+
+                    for (var row in trainsQuery.rows) {
+                      var trainID = row.colByName("ID")!;
+
+                      var searchResult = (await dbModel.conn.execute("""
+    SELECT 
+        pt1.TrainID, pt1.Time AS S_Time, pt2.Time AS F_Time, t.NameEN, t.NameAR, t.BusinessCapacity, t.EconomyCapacity, t.StartsAt_Name, t.EndsAt_Name, t.StartsAt_Time
+    FROM
+        passing_through pt1 JOIN passing_through pt2 ON pt1.TrainID = pt2.TrainID
+            JOIN train t ON pt1.TrainID = t.ID
+    WHERE
+        pt1.TrainID = $trainID AND pt1.StationName = t.StartsAt_Name AND pt2.StationName = t.EndsAt_Name AND pt1.SequenceNo < pt2.SequenceNo 
+    ORDER BY pt1.TrainID, pt1.SequenceNo;
+  """)).rows.toList();
+
+                      var bookings = (await dbModel.conn.execute("""
+    SELECT *
+    FROM booking NATURAL JOIN listed_booking
+    WHERE
+    Date = '${pickedDate.year}-${pickedDate.month}-${pickedDate.day}';
+  """)).rows.toList();
+
+                      var ptRow = searchResult.first;
+
+                      tmpCardsData.add(TrainCardData(
+                        trainID: int.parse(ptRow.colByName("TrainID")!),
+                        nameEN: ptRow.colByName("NameEN")!,
+                        nameAR: ptRow.colByName("NameAR")!,
+                        source: ptRow.colByName("StartsAt_Name")!,
+                        destination: ptRow.colByName("EndsAt_Name")!,
+                        date: "${pickedDate.year}-${pickedDate.month}-${pickedDate.day}",
+                        sTime: int.parse(ptRow.colByName("S_Time")!),
+                        fTime: int.parse(ptRow.colByName("F_Time")!),
+                        businessCapacity: int.parse(ptRow.colByName("BusinessCapacity")!),
+                        economyCapacity: int.parse(ptRow.colByName("EconomyCapacity")!),
+                        bookedBusiness: bookings.where((booking) => booking.colByName("On_ID") == ptRow.colByName("TrainID") && booking.colByName("Coach") == "Business").length,
+                        bookedEconomy: bookings.where((booking) => booking.colByName("On_ID") == ptRow.colByName("TrainID") && booking.colByName("Coach") == "Economy").length,
+                      ));
                     }
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey.shade300),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          selectedDate == null ? "Select Date" : "${selectedDate!.toLocal()}".split(" ")[0],
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        const Icon(Icons.calendar_today),
-                      ],
-                    ),
+
+                    //by train ID
+                    tmpCardsData.sort((a, b) => a.trainID.compareTo(b.trainID));
+
+                    setState(() {
+                      selectedDate = pickedDate;
+                      cardsData = tmpCardsData;
+                    });
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey.shade300),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        selectedDate == null ? "Select Date" : "${selectedDate!.toLocal()}".split(" ")[0],
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const Icon(Icons.calendar_today),
+                    ],
                   ),
                 ),
               ),
             ),
-        
+          ),
           const SizedBox(height: 20),
           Expanded(
             child: Container(

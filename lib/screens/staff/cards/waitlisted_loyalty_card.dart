4116a0554b15bd1f554c38cf0d6a349bf3b340e2
ismael-782 <ismaelireqsosy@gmail.com>
2024-12-11 23:49:39 +0300

    waitlist card design

--- lib/screens/staff/cards/waitlisted_loyalty_card.dart
+++ lib/screens/staff/cards/waitlisted_loyalty_card.dart
@@ -9,8 +9,6 @@ class WaitlistedLoyaltyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var passengerLoyaltyLevel;
-
     return Container(
       decoration: BoxDecoration(
         color: const Color.fromARGB(255, 255, 255, 255),
@@ -34,27 +32,50 @@ class WaitlistedLoyaltyCard extends StatelessWidget {
             ),
             const SizedBox(width: 10),
             Container(
-                height: 20,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: passengerLoyaltyLevel == "Temp"
-                      ? Colors.blue[100]
-                      : passengerLoyaltyLevel == "Paid"
-                          ? Colors.green[100]
-                          : passengerLoyaltyLevel == "Waitlisted"
-                              ? Colors.orange[100]
-                              : Colors.red[100],
-                  border: Border.all(
-                      color: passengerLoyaltyLevel == "Temp"
-                          ? Colors.blue
-                          : passengerLoyaltyLevel == "Paid"
-                              ? Colors.green
-                              : passengerLoyaltyLevel == "Waitlisted"
-                                  ? Colors.orange
-                                  : Colors.red),
-                  borderRadius: BorderRadius.circular(5),
+              height: 20,
+              width: 100,
+              decoration: BoxDecoration(
+                color: milesTraveled >= 100000
+                    ? Colors.yellow[700] ?? Colors.yellow // Gold
+                    : milesTraveled >= 50000
+                        ? Colors.grey[400] ?? Colors.grey // Silver
+                        : milesTraveled >= 10000
+                            ? Colors.green[400] ?? Colors.green // Green
+                            : Colors.red[400] ?? Colors.red, // Default color
+                border: Border.all(
+                  color: milesTraveled >= 100000
+                      ? Colors.yellow[800] ?? Colors.yellow // Gold border
+                      : milesTraveled >= 50000
+                          ? Colors.grey[600] ?? Colors.grey // Silver border
+                          : milesTraveled >= 10000
+                              ? Colors.green[700] ?? Colors.green // Green border
+                              : Colors.red, // Default border
                 ),
-                child: Text(passengerLoyaltyLevel, textAlign: TextAlign.center)),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(
+                child: Text(
+                  milesTraveled >= 100000
+                      ? "Gold"
+                      : milesTraveled >= 50000
+                          ? "Silver"
+                          : milesTraveled >= 10000
+                              ? "Green"
+                              : "None", // Default text
+                  style: TextStyle(
+                    color: milesTraveled >= 100000
+                        ? Colors.yellow[800] ?? Colors.yellow // Gold text
+                        : milesTraveled >= 50000
+                            ? Colors.grey[600] ?? Colors.grey // Silver text
+                            : milesTraveled >= 10000
+                                ? Colors.green[700] ?? Colors.green // Green text
+                                : Colors.red, // Default text color
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
           ],
         ),
       ),
